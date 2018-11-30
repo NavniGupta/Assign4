c86bdf9d335391d7f2953dfbf5edf5e97c1f0448
NavniGupta <43049801+NavniGupta@users.noreply.github.com>
2018-11-30 17:17:33 -0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -96,8 +96,8 @@ clockid_t clk_id;
             perror ("clock_gettime():");
         }
         else
-        { printf (" %ld \n",
-                     tspec.tv_sec+(tspec.tv_nsec/1000000000));
+        {  printf ("%ld.\n",
+                     tspec.tv_sec+tspec.tv_nsec/1000000000);
         }
     }
     else
